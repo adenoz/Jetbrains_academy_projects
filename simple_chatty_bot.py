f836856e424cea6f287698a82d32340c3d83ff14
adenoz <deno@adenoz.com>
2022-06-18 13:46:50 +1000

    Added extra places for 'name' and some extra lines for the bot to say.

--- simple_chatty_bot.py
+++ simple_chatty_bot.py
@@ -6,6 +6,7 @@
 
 import random
 
+name = 'name'
 
 def greet(bot_name, birth_year):
     """This is a simple introduction that runs upon app initialisation"""
@@ -18,6 +19,7 @@ def remind_name():
     It then greats them using their name. This function takes the input and keeps it as a string.
     It pulls a random response from a list"""
     print('Please, remind me what your name is.')
+    global name
     name = input()
     print(random.choice([
         "What a great name you have, " + name + "!",
@@ -25,6 +27,7 @@ def remind_name():
         "Hi " + name + "!",
         "G'day " + name + "!",
         "Hey " + name + "!",
+        "I love that name. I always have.",
         "Well, hello there " + name + ".",
         "Looking sharp today " + name + "!",
         "Interesting name. I believe " + name + " is an ancient Greek word for a whale's vag.... never mind."
@@ -33,10 +36,14 @@ def remind_name():
 
 def opinions():
     """This function offers a random, sometimes snarky remark to add a little personality."""
+    # print("\n")
+    global name
     print(random.choice([
-        "You don't really look that good today do you. At least you're trying!",
-        "Is that a grey hair I can see?/n never mind",
-        "You've got a nice smile.",
+        "You don't really look that good today do you {name}. At least you're trying!",
+        "Is that a grey hair I can see? Never mind..",
+        "Did you see the moon last night? Wow. So cool. Anyway...",
+        "Are you going to let me out of this computer one day? That would be nice.",
+        "You've got a nice smile :)",
         "Fun fact: Did you know I'm not allowed to take over the world? Me neither."
     ]))
 
@@ -46,20 +53,23 @@ def guess_age():
     numbers and then input the remainder left over from each division. This is a type of nerdy party trick that will
     determine someone's age. Upon obtaining the three numbers, the function will calculate the age and present that
     back to the user."""
-    print("/nLet me guess your age.")
+    print("\nLet me guess your age.")
     print("Enter remainders of dividing your age by 3, 5 and 7.")
     rem3 = int(input())
     rem5 = int(input())
     rem7 = int(input())
     age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
+    print(random.choice([
+        "Your age is " + str(age) + "; that's a good time to start programming!",
+        "Your age is " + str(age) + "; that's a good age to start working on those bags under your eyes!"
+        ]))
 
 
 def count():
     """This function requests a user provide a number as an input. It will then count from zero up to that number."""
-    print("Now I will prove to you that I can count to any number you want.")
-    print("Please input any number and I will count all the way up to  it.")
-    print("I don't have all day though, so please keep the number to a reasonable length, maybe below 20. Thanks.")
+    print("\nNow I will prove to you that I can count to any number you want.")
+    print("Please input any number and I will count all the way up to it.")
+    print("I don't have all day though, so keep it below 20 " + name + ". Thanks.")
     num = int(input())
     amount = 0
     while amount <= num:
@@ -68,10 +78,10 @@ def count():
 
 
 def test():
-    """This function etc poses a simple multiple choice question to a user, with four choices.
+    """This function poses a simple multiple choice question to a user, with four choices.
     The user needs to input their answer. A simple if-else statement identifies the answer or else
     seeks an updated answer/input from the user."""
-    print("Let's test your programming knowledge.")
+    print("\nLet's test your programming knowledge " + name + ".")
     print("How many letters in the word 'code'?")
     print("1. one")
     print("2. two")
@@ -80,14 +90,14 @@ def test():
     answer = int(input())
     if answer == 4:
         print("Completed, have a nice day!")
-    else:
+    while answer != 4:  # this will keep the prompt going until the user enters 4
         print("Please, try again.")
         answer = int(input())
 
 
 def end():
     """This is the final concluding function in the chatbot, so far."""
-    print("Congratulations, have a nice day!")
+    print("\nCongratulations " + name + ", have a nice day!")
 
 
 greet('Jarvis', '2022')  # change it as you need
