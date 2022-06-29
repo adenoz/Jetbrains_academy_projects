3b44fbd9d90320aeb30ef10be43637d2832ddb23
adenoz <deno@adenoz.com>
2022-06-29 11:02:11 +1000

    This is now complete for stage 3 of 4.

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,7 +1,6 @@
-# THIS REMAINS A WORK IN PROGRESS!
-# Notes. I just completed the initial draft calculations and variable assignments.
-# They remain raw. So I next need to consider rounding up or to confirm float v int etc.
-# I also haven't fully confirmed all calculations are complete or correct. Here.
+
+# This is the Loan calculator project in the python track in Jetbrains / Hyperskill
+# Currently it is at stage 3 of 4.
 
 import math
 
@@ -17,7 +16,7 @@ if want != 'p':
 
 if want != 'a':
     print("Enter the monthly payment:")
-    m_payment = int(input())
+    m_payment = float(input())
 
 if want != 'n':
     print("Enter the number of periods:")
@@ -28,51 +27,42 @@ interest = float(input())
 
 # DATA SHAPING AND WRANGLING
 
-loan_interest = interest / 12 * 100
+loan_interest = interest / (12 * 100)
 
-principal_calc = m_payment / (loan_interest * (1 + loan_interest)**n_periods / (1 + loan_interest)**n_periods - 1)
+# We only need to use the input figures for all calculations below.
+# Do not use any calculated figures.
 
-m_payment_calc = principal * (loan_interest * (1 + loan_interest)**n_periods / (1 + loan_interest)**n_periods - 1)
+if want == 'p':  # this is the loan principal
+    bottom_top = loan_interest * (1 + loan_interest)**n_periods
+    bottom_bottom = (1 + loan_interest)**n_periods - 1
+    principal_calc = m_payment / (bottom_top / bottom_bottom)
 
-n_periods_calc = math.log(1 + loan_interest) * (m_payment / m_payment - loan_interest * principal)
+if want == 'a':  # this is the monthly annuity payment
+    top = loan_interest * (1 + loan_interest)**n_periods
+    bottom = (1 + loan_interest)**n_periods - 1
+    m_payment_calc = principal * (top / bottom)
 
-n_rounded = round(n_periods_calc)  # Is using the round() function what I was thinking here?
-
-if n_rounded <= 12:
-    n_month_only == n_rounded
-elif n_rounded > 12:
-    n_years = n_rounded // 12
-    n_months = n_rounded % 12
-
-time = principal / m_payment
+if want == 'n':  # this is the number of monthly payments
+    main = (m_payment / (m_payment - (loan_interest * principal)))
+    to_log = 1 + loan_interest
+    n = math.log(main, to_log)
+    n_rounded = math.ceil(n)
+    if n_rounded <= 12:
+        n_month_only = n_rounded
+    elif n_rounded > 12:
+        n_years = n_rounded // 12
+        n_months = n_rounded % 12
 
 # THE OUTPUTS
 
 if want == 'p':
-print(f"Your loan principal = {HERE}!")
+    print(f"Your loan principal = {round(principal_calc)}!")
 
 if want == 'a':
-    print(f"Your monthly payment = {HERE}!")
+    print(f"Your monthly payment = {math.ceil(m_payment_calc)}!")
 
 if want == 'n':
     if n_rounded < 13:
         print(f"It will take {n_month_only} months to repay this loan!")
     elif n_rounded > 12:
         print(f"It will take {n_years} years and {n_months} months to repay this loan!")
-
-
-
-###---------------------------
-# DELETE BELOW LATER
-
-if time <= 1:
-    print(f"It will take {math.ceil(time)} month to repay the loan")
-elif time > 1:
-    print(f"It will take {math.ceil(time)} months to repay the loan")
-
-if want == 'p':
-    print("Enter the number of months:")
-    n_months = int(input())
-    payment = principal / n_months
-    last = principal - (n_months - 1) * math.ceil(payment)
-    print(f"Your monthly payment = {math.ceil(payment)} and the last payment = {math.ceil(last)}.")
